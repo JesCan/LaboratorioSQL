1e322b7f7458e052ee0bc840994490034e6b308c
Monitor <monitor@vallegrande.edu.pe>
2018-05-26 10:38:58 -0500

    Terminamos el script sobre manejo de BDs en MS SQL Server

--- Lab01/QueryOne.sql
+++ Lab01/QueryOne.sql
@@ -14,3 +14,34 @@ GO
 /* Ver listado de tablas de una base de datos */
 Use AdventureWorks2016
 SELECT name from sys.tables;
+
+/* Ver listado de vistas de una base de datos */
+USE AdventureWorks2016;
+SELECT NAME FROM SYS.views;
+GO
+
+/* Visualizar la información detallada de una base de datos */
+EXEC sp_helpdb AdventureWorks2016;
+
+/* Utilizando snippets */
+-- Create a new database called 'MercadoVG'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'MercadoVG'
+)
+CREATE DATABASE MercadoVG
+GO
+
+/* Eliminar una Base de Datos */
+DROP DATABASE HospitalHHH;
+GO
+
+/* Eliminar base de datos AdventureWORKS */
+DROP DATABASE AdventureWorks2016;
+DROP DATABASE AdventureWorksDW2016;
+GO
